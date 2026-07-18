fbb245a954d5aefcb51df7b25cf3ea70650999ff
Surendhar <surendhar00740@gmail.com>
2026-07-18 20:44:56 +0530

    data types in js

---
+++ fundamentals/dataTypes.js
@@ -0,0 +1,45 @@
+//Data Types are of 2 types: Primitive and Non-Primitive(Reference Types or Object Types)
+
+// Primitive Data Types: Number, BigInt, String, Boolean, Undefined, Null, Symbol
+
+// Number
+let length = 16;
+let weight = 7.5;
+
+// BigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345)
+
+// Strings
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Boolean
+let x = true;
+let y = false;
+
+// Undefined
+let x;
+let y;
+
+// Null
+let x = null;
+let y = null;
+
+// Symbol
+const x = Symbol();
+const y = Symbol();
+
+
+// Non-Primitive Data Types: Object, Array, Function
+
+// Object
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Function
+function myFunction() {
+  return "Hello World!";
+}
